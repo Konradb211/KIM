61b27c246a2e8d6bee558c43a945ff0ce9b57d54
Konrad Baś <konradbas@192.168.1.8>
2023-04-17 17:49:39 +0200

    idk what

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const nav = document.querySelector(".navbar-mobile")
 const navBtn = document.querySelector(".burger-btn")
 const allNavItems = document.querySelectorAll(".navbar-mobile__link")
+const offersBoxOne = document.querySelector(".offers__box-one")
+const offers = document.querySelector("#offers")
+console.log(offers)
 
 const navAnim = () => {
 	nav.classList.toggle("navbar-mobile--active")
@@ -11,5 +14,12 @@ const navAnim = () => {
 		})
 	})
 }
+
+// const paralax = () => {
+// 	const value = offers.scrollY
+// 	offersBoxOne.style.top = value * 2 + "px"
+// }
+
+// window.addEventListener("scroll", paralax)
 
 navBtn.addEventListener("click", navAnim)
