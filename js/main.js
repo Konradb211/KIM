20b86d853b4a863c0380906eabae857be5a42122
Konrad Baś <konradbas@192.168.1.8>
2023-04-18 22:45:00 +0200

    added new section

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 const nav = document.querySelector(".navbar-mobile")
 const navBtn = document.querySelector(".burger-btn")
 const allNavItems = document.querySelectorAll(".navbar-mobile__link")
-const offersBoxOne = document.querySelector(".offers__box-one")
-const offers = document.querySelector("#offers")
 
 const navAnim = () => {
 	nav.classList.toggle("navbar-mobile--active")
